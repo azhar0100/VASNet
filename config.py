997468525819b174f93fd0aec070fc359f0af05a
azhar <azhartahir0100@gmail.com>
2019-08-15 01:01:57 +0500

    Changed hyperparameters to run on device 1

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class HParameters:
     def __init__(self):
         self.verbose = False
         self.use_cuda = True
-        self.cuda_device = 0
+        self.cuda_device = 1
         self.max_summary_length = 0.15
 
         self.l2_req = 0.00001
