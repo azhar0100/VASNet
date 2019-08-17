affac5b131953dd64abe08b2fac87aaef6b540e2
azhar <azhartahir0100@gmail.com>
2019-08-17 10:21:43 +0500

    Added ax script

---
+++ main_transformer_ax.py
@@ -0,0 +1,85 @@
+:from main_transformer import *
+
+import numpy as np
+
+from ax.plot.contour import plot_contour
+from ax.plot.trace import optimization_trace_single_method
+from ax.service.managed_loop import optimize
+from ax.metrics.branin import branin
+from ax.utils.measurement.synthetic_functions import hartmann6
+from ax.utils.notebook.plotting import render, init_notebook_plotting
+
+fixed_d = {
+    "splits" : ['splits/tvsum_aug_splits_quick.json',
+                    'splits/summe_aug_splits_quick.json']
+}
+
+d = {
+    n_heads : 4,
+    l2_req : 0.00001,
+    lr_epochs : [0],
+    lr : [0.00005],
+    epochs_max : 500,
+}
+
+print_pkg_versions()
+def eval_s(d):
+    hps = HParameters()
+
+    d.update(fixed_d)
+    d['lr'] = [d['lr']]
+    hps.load_from_args(d)
+
+    print("Parameters:")
+    print("----------------------------------------------------------------------")
+    print(hps)
+
+    scores = []
+
+    if hps.train:
+        train(hps)
+    else:
+        results=[['No', 'Split', 'Mean F-score']]
+        for i, split_filename in enumerate(hps.splits):
+            f_score = eval_split(hps, split_filename, data_dir=hps.output_dir)
+            scores.append(f_score)
+            results.append([i+1, split_filename, str(round(f_score * 100.0, 3))+"%"])
+
+        print("\nFinal Results:")
+        print_table(results)
+
+    return {"f_score_tvsum": (scores[0], 0.0), "f_score_summe": (scores[1], 0.0) }
+
+best_parameters, values, experiment, model = optimize(
+    parameters=[
+        {
+            "name": "n_heads",
+            "type": "choices",
+            "choices": [2, 4, 8],
+            "value_type": "int",  # Optional, defaults to inference from type of "bounds".
+        },
+        {
+            "name": "l2_req",
+            "type": "range",
+            "bounds": [0.0, 0.1],
+            "log_scale": True
+        },
+        {
+            "name": "lr",
+            "type": "range",
+            "bounds": [0.0, 1.0],
+            "log_scale":True
+        }
+        {
+            "name": "epochs_max",
+            "type": "range",
+            "bounds": [0, 500],
+        }
+    ],
+    experiment_name="test",
+    objective_name="evaluation",
+    evaluation_function=eval_s,
+    total_trials=50, # Optional.
+)
+
+pickle.dump([best_parameters, values, experiment, model], open("saved_optim"), protocol=None, *, fix_imports=True)
